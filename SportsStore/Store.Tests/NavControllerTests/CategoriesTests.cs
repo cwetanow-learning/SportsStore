0c7e11f854d621ecd20cedad70f27bb6900e51a3
Ivan <ivan.mladenov@abbaty.int>
2016-08-15 12:37:36 +0300

    test for category selection

--- SportsStore/Store.Tests/NavControllerTests/CategoriesTests.cs
+++ SportsStore/Store.Tests/NavControllerTests/CategoriesTests.cs
@@ -2,6 +2,7 @@
 using NUnit.Framework;
 using Store.Domain.Contracts;
 using Store.WebUI.Controllers;
+using Store.WebUI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -76,6 +77,40 @@ namespace Store.Tests.NavControllerTests
             var result = controller.Menu(selectedCategory).ViewBag.SelectedCategory;
 
             Assert.AreEqual(result, selectedCategory);
+        }
+
+        [TestCase("Cat1")]
+        [TestCase("Cat2")]
+        [TestCase(null)]
+        public void TestCategories_ShouldGroupByCategoryCorrectly(string category)
+        {
+            var mockedRepository = new Mock<IProductRepository>();
+
+            var firstMockedProduct = new Mock<IProduct>();
+            firstMockedProduct.SetupGet(p => p.ProductID).Returns(1);
+            firstMockedProduct.SetupGet(p => p.Name).Returns("P1");
+            firstMockedProduct.SetupGet(p => p.Category).Returns("Cat1");
+
+            var secondMockedProduct = new Mock<IProduct>();
+            secondMockedProduct.SetupGet(p => p.ProductID).Returns(2);
+            secondMockedProduct.SetupGet(p => p.Name).Returns("P2");
+            secondMockedProduct.SetupGet(p => p.Category).Returns("Cat2");
+
+            var thirdtMockedProduct = new Mock<IProduct>();
+            thirdtMockedProduct.SetupGet(p => p.ProductID).Returns(3);
+            thirdtMockedProduct.SetupGet(p => p.Name).Returns("P3");
+            thirdtMockedProduct.SetupGet(p => p.Category).Returns("Cat1");
+
+            var listOfProducts = new List<IProduct> { firstMockedProduct.Object, secondMockedProduct.Object, thirdtMockedProduct.Object };
+
+            mockedRepository.Setup(s => s.Products).Returns(listOfProducts);
+
+            var controller = new ProductController(mockedRepository.Object);
+
+            var result = ((ProductsListViewModel)controller.List(category).Model).PagingInfo.TotalItems;
+            var expected = mockedRepository.Object.Products.Where(c => category == null || c.Category == category).Count();
+
+            Assert.AreEqual(result, expected);
         }
     }
 }
